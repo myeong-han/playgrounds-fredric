6c31b0b663933b0affae410a080931b227034773
myeong-han <ames11118@gmail.com>
2023-12-04 12:39:58 +0000

    kong-istio test

---
+++ iac/bash/namespaces.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/mhkim/playgrounds-fredric/iac/helm
+export nss=$(ls -al | grep -Evw "README.md" | sed '1,3d' | awk '{print $9}' | sed -z 's/[\n\r]/ /g')
+
+for ns in $nss 
+do
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  creationTimestamp: null
+  name: ${ns}
+spec: {}
+status: {}
+---
+EOF
+done
---
+++ iac/cluster/setup/get_krew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# krew: plugin-manager 설치
+# Krew 설치
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+# .bashrc or .zshrc에 추가
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> .bashrc
+
+# shell 반영
+source $HOME/.bashrc
+
+# 패키지 설치
+kubectl krew install ns
