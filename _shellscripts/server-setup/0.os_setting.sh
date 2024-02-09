28dd2775d0bf81eb3e839eec400df8ce22a5e304
myeong-han <ames11118@gmail.com>
2024-02-09 23:27:48 +0900

    init palworld server

---
+++ _shellscripts/server-setup/0.os_setting.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#타임존 설정
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
--- iac/1.manifests/generate-kube-manifests/2.tls.sh
+++ iac/1.manifests/generate-kube-manifests/2.tls.sh
@@ -1,4 +1,5 @@
 cert=/etc/ssl/fredric/server.pem
 key=/etc/ssl/fredric/server.key
 
+kubectl delete secret -n cicd harbor.fredric.playground.cld-tls
 kubectl create secret tls -n cicd harbor.fredric.playground.cld-tls --cert=$cert --key=$key
