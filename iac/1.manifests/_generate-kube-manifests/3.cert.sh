3339b18f9c65e754799984c8aa2d8449a2339f63
myeong-han <ames11118@gmail.com>
2024-03-07 17:40:14 +0900

    develop tls-shellfiles

--- iac/1.manifests/_generate-kube-manifests/3.cert.sh
+++ iac/1.manifests/_generate-kube-manifests/3.cert.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+# Copyright March 7, 2024 Myeong-han Kim.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #validation1
 if [ -z $1 ]; then
 	echo "##Report: Certificate Name(Secret Name)을 첫번째 파라미터로 입력해야 동작합니다."
-		        exit 1
+	exit 1
 fi
 #validation1
 if [ -z $2 ]; then
-	        echo "##Report: Namespace명을 두번째 파라미터로 입력해야 동작합니다."
-		        exit 1
+	echo "##Report: Namespace명을 두번째 파라미터로 입력해야 동작합니다."
+	exit 1
 fi
 
+echo "##Report: 세번째 파라미터를 입력할 경우 해당 도메인에 대한 인증서 발급, 입력하지 않을 경우 env에 등록된 모든 도메인에 대한 인증서 발급."
 echo "#############################################"
-echo "###Create> bash $0 $1 $2 | kubectl apply -f -"
+echo "###exec> bash $0 $1 $2 $3 | kubectl apply -f -"
 echo "#############################################"
 
 # Certificate의 이름과 네임스페이스를 변수로 지정
@@ -36,9 +50,13 @@ spec:
   dnsNames:
 EOF
 
-# DNS 도메인들을 반복하여 추가
-for domain in "${DOMAINS[@]}"; do
-	echo "  - $domain"
-done
+if [ -z $3 ]; then
+	# DNS 도메인들을 반복하여 추가
+	for domain in "${DOMAINS[@]}"; do
+		echo "  - $domain"
+	done
+else
+	echo "  - $3"
+fi
 # 마지막 부분에 다시 EOF를 붙여서 종료
 echo "---"
